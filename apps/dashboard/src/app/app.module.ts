2f79ba0e74e9b380b96ec71756eb2541ec3a1f70
eccentricnode <shival.bigman@gmail.com>
2019-08-19 19:28:17 -0700

    incremental checkin

--- apps/dashboard/src/app/app.module.ts
+++ apps/dashboard/src/app/app.module.ts
@@ -1,12 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { NxModule } from '@nrwl/angular';
+import { CoreDataModule } from '@second-pass/core-data';
+import { CoreStateModule } from '@second-pass/core-state';
+import { MaterialModule } from '@second-pass/material';
 
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './routing.module';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule],
+  declarations: [AppComponent, LoginComponent],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    NxModule.forRoot(),
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    CoreDataModule,
+    CoreStateModule,
+    MaterialModule,
+    AppRoutingModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ apps/dashboard/src/app/routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+
+const routes: Routes = [
+    { path: '', redirectTo: '/login', pathMatch: 'full' },
+    { path: '', component: LoginComponent },
+    { path: 'path', component: FeatureComponent },
+    { path: '**', redirectTo: '/login', pathMatch: 'full' },
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule {}
